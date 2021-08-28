2a5ed6da05addfe64973e892d9d52b98a5e3e86d
omar-etman <omartareketman@gmail.com>
2021-08-28 18:12:35 +0200

    Setup the Repo component preliminary UI

--- github-repos/src/components/Repo.js
+++ github-repos/src/components/Repo.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import '../styles/Repo.css'
 
 function Repo() {
     return (
-        <div>
-            <p>repo</p>
+        <div className = "repo">
+           <div className="repo__avatar">
+               img
+           </div>
+           <div className="repo__info">
+               <div className="info__title">
+                   title
+               </div>
+               <div className="info__description">
+                   description
+               </div>
+               <div className="info__stats">
+                   <div className="info__stats__numbers">
+                       <div>stars</div>
+                       <div>issues</div>
+                   </div>
+                   <div className="info__stats__timestamp">
+                       timestamp
+                   </div>
+               </div>
+           </div>
+
         </div>
     )
 }
--- github-repos/src/components/ReposList.js
+++ github-repos/src/components/ReposList.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Repo from './Repo'
 
 function ReposList() {
+
     return (
-        <div>
+        <div className="repo">
             <p>ReposList</p>
             <ul>
+                <li><Repo/></li>
+                <li><Repo/></li>
+                <li><Repo/></li>
                 <li><Repo/></li>
             </ul>
         </div>
